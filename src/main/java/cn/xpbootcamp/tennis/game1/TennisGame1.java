c677d6b223a3c92a8216947b14b8faf1d27bf4ba
vermouth <vermouth_fee@outlook.com>
2020-03-04 10:13:59 +0800

    [Long Method]: extract function

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -21,53 +21,69 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         StringBuilder score = new StringBuilder();
-        int tempScore;
         if (player1Score == player2Score) {
-            switch (player1Score) {
-                case 0:
-                    score = new StringBuilder("Love-All");
-                    break;
-                case 1:
-                    score = new StringBuilder("Fifteen-All");
-                    break;
-                case 2:
-                    score = new StringBuilder("Thirty-All");
-                    break;
-                default:
-                    score = new StringBuilder("Deuce");
-                    break;
-
-            }
+            score = getScoreResultOfEqualScores(player1Score);
         } else if (player1Score >= 4 || player2Score >= 4) {
-            int minusResult = player1Score - player2Score;
-            if (minusResult == 1) score = new StringBuilder("Advantage player1");
-            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
-            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
+            score = getScoreResultOfAnyScoreGreaterThanFour(player1Score, player2Score);
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) {
-                    tempScore = player1Score;
-                } else {
-                    score.append("-");
-                    tempScore = player2Score;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
-            }
+            getScoreResultOfCommon(score, player1Score, player2Score);
         }
         return score.toString();
+    }
+
+    private static void getScoreResultOfCommon(StringBuilder score, int player1Score, int player2Score) {
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = player1Score;
+            } else {
+                score.append("-");
+                tempScore = player2Score;
+            }
+            switch (tempScore) {
+                case 0:
+                    score.append("Love");
+                    break;
+                case 1:
+                    score.append("Fifteen");
+                    break;
+                case 2:
+                    score.append("Thirty");
+                    break;
+                case 3:
+                    score.append("Forty");
+                    break;
+            }
+        }
+    }
+
+    private static StringBuilder getScoreResultOfAnyScoreGreaterThanFour(int player1Score, int player2Score) {
+        StringBuilder score;
+        int minusResult = player1Score - player2Score;
+        if (minusResult == 1) score = new StringBuilder("Advantage player1");
+        else if (minusResult == -1) score = new StringBuilder("Advantage player2");
+        else if (minusResult >= 2) score = new StringBuilder("Win for player1");
+        else score = new StringBuilder("Win for player2");
+        return score;
+    }
+
+    private static StringBuilder getScoreResultOfEqualScores(int player1Score) {
+        StringBuilder score;
+        switch (player1Score) {
+            case 0:
+                score = new StringBuilder("Love-All");
+                break;
+            case 1:
+                score = new StringBuilder("Fifteen-All");
+                break;
+            case 2:
+                score = new StringBuilder("Thirty-All");
+                break;
+            default:
+                score = new StringBuilder("Deuce");
+                break;
+
+        }
+        return score;
     }
 }
